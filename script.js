194a17da343238568a8a6760a398554032ffdcc7
Saurabh Singh <saurabhsingh121@gmail.com>
2020-09-14 00:34:33 +0530

    caching with localstorage

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -62,7 +63,11 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
-
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+  localStorage.setItem("countdown", JSON.stringify(savedCountdown));
   // Check for valid date
   if (countdownDate === "") {
     alert("Please select a date for the countdown.");
@@ -85,9 +90,25 @@ function reset() {
   // Reset values
   countdownTitle = "";
   countdownDate = "";
+  localStorage.removeItem("cound");
+}
+
+function restorePreviousCountdown() {
+  // Get countdown from localstorage if available
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
 }
 
 // Event Listeners
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", reset);
 completeBtn.addEventListener("click", reset);
+
+// On load, check local storage
+restorePreviousCountdown();
